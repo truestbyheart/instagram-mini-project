7da9beb5ed9182e747690ea5a45aa45df44681c1
daniel mwangila <margalight@outlook.com>
2020-12-28 13:04:51 +0200

    resolved build issues

--- src/Modules/Post/post.controller.ts
+++ src/Modules/Post/post.controller.ts
@@ -26,6 +26,9 @@ class PostController {
       const mediaObject = graphQLJSON.entry_data.PostPage[0].graphql.shortcode_media;
       const filteredResult: any = filterIgData(mediaObject);
 
+      //close the browser
+      await puppeteerService.closeBrowser(browser);
+
       // sorting through the JSON object
       res.status(OK).json({
         status: OK,
