4f2f7f41df616e5dd8183f0469001a140a6e9dfa
daniel mwangila <margalight@outlook.com>
2020-12-28 13:04:51 +0200

    resolved build issues

--- src/Config/app.config.ts
+++ src/Config/app.config.ts
@@ -6,7 +6,7 @@ const environment = process.env.NODE_ENV?.trim() || 'development';
 const dir =
   environment === 'development' ? path.join(__dirname, '../../.env.development') : path.join(__dirname, '../../.env');
 
-const result = dotenv.config({ path: dir });
+dotenv.config({ path: dir });
 
 export const TOKEN_KEY: any = process.env.TOKEN_KEY;
 export const HEADLESS_STATUS: any = process.env.HEADLESS_STATUS;
